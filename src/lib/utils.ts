fa63c7f7b8a8bfacdb8b58fc9a1666e0678e32e4
Mr Bushido <114846273+fitzies@users.noreply.github.com>
2024-10-07 08:45:13 +0800

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -189,3 +189,15 @@ export function filterResults(
 
   return statuses;
 }
+
+function sort_by_id() {
+        return function (elem1, elem2) {
+          if (elem1.id < elem2.id) {
+            return -1;
+          } else if (elem1.id > elem2.id) {
+            return 1;
+          } else {
+            return 0;
+          }
+        };
+      }
