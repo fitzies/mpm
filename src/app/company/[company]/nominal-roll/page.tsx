37864eec63e6016d91c79008477f67400beb17de
fitzies <olicrypt@gmail.com>
2024-10-24 15:10:39 +0800

    added polar tick and changed status loading

--- src/app/company/[company]/nominal-roll/page.tsx
+++ src/app/company/[company]/nominal-roll/page.tsx
@@ -1,14 +1,11 @@
 import PageWrapper from "@/components/page-wrapper";
 import { getActiveStatuses, getCompany } from "@/lib/db";
-import { DataTableType, RecruitWithStatuses } from "../../../../../types";
+import { DataTableType } from "../../../../../types";
 import NR from "@/components/nr";
 import { StatusType } from "@prisma/client";
-import {
-  checkRecruitOutOfCamp,
-  getLatestConduct,
-  parseDate,
-} from "@/lib/utils";
+import { checkRecruitOutOfCamp, getLatestConduct } from "@/lib/utils";
 import { Badge } from "@/components/ui/badge";
+import { Check, X } from "lucide-react";
 
 export default async function Page({
   params,
@@ -36,30 +33,20 @@ export default async function Page({
   );
 
   const tableData: DataTableType = {
-    headers: ["4D", "Name", "SOC", "RM", "HA", "Booked in"],
+    headers: ["4D", "Name", "SOC", "RM", "HA", "Polar", "Booked in"],
     rows: recruits.map((recruit) => [
       recruit.id,
       recruit.name,
       getLatestConduct(recruit, "SOC"),
       getLatestConduct(recruit, "RouteMarch"),
       "Nil",
-      // recruit.statuses
-      //   .map((status) => {
-      //     if (
-      //       parseDate(status.startDate) <= new Date() &&
-      //       parseDate(status.endDate) >= new Date() &&
-      //       (status.type === StatusType.BookedOut ||
-      //         status.type === StatusType.MC ||
-      //         status.type === StatusType.Other)
-      //     ) {
-      //       console.log(status.type);
-      //       return status.type === "Other" ? status.remarks : status.type;
-      //     } else {
-      //       return null;
-      //     }
-      //   })
-      //   .filter((status) => status !== null)
-      //   .join(", "),
+      <div className="opacity-50 hover:opacity-100">
+        {recruit.polarUserId && recruit.polarAccessToken ? (
+          <Check className="scale-75 " />
+        ) : (
+          <X className="scale-75 " />
+        )}
+      </div>,
       checkRecruitOutOfCamp(recruit.id, statuses) ? (
         <Badge variant={"destructive"}>No</Badge>
       ) : (
